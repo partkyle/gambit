165bd7bc2bd98be8a8f3c11d162accc8ecdc177b
Kyle Partridge <kyle.partridge@sendgrid.com>
2012-07-06 16:51:45 -0700

    style changes

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -20,10 +20,10 @@ $(function() {
       players = data.players;
       var html = '<ul class="unstyled">';
       for (var player in players) {
-        html += '<li class="clearfix shelf">';
+        html += '<li class="clearfix shelf" data-player-id="' + player + '">';
         html += '<span class="pull-left">' + players[player].name + '</span>';
         if (players[player].score) {
-          html += '<span class="pull-right">' + players[player].score + '</span>';
+          html += '<span class="pull-right end-round">' + players[player].score + '</span>';
         }
         html += '</li>';
       }
